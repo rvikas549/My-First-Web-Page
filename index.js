535a543292d63c84c26222cb315c5649a1f19575
Vikas R <153126314+rvikas549@users.noreply.github.com>
2024-06-15 00:28:40 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+document.querySelectorAll("button")[0].addEventListener("click",function(){
+    alert("9844xxx");
+});
+
+document.querySelector(".viki").addEventListener("click",function()
+{
+    alert("This is my nickname!!");
+});
